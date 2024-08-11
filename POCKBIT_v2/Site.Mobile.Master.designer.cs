5f371155e2989561545b8a79e409103cb0e8df34
César Enriquez <136645895+CesarS39@users.noreply.github.com>
2024-08-11 17:46:58 -0600

    Modificacion mobile navbar
    se agrego los links de la navbar para pc a la version mobile

--- POCKBIT_v2/Site.Mobile.Master.designer.cs
+++ POCKBIT_v2/Site.Mobile.Master.designer.cs
@@ -24,24 +24,6 @@ namespace POCKBIT_v2
         protected global::System.Web.UI.WebControls.ContentPlaceHolder HeadContent;
 
         /// <summary>
-        /// Control form1.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-
-        /// <summary>
-        /// Control FeaturedContent.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder FeaturedContent;
-
-        /// <summary>
         /// Control MainContent.
         /// </summary>
         /// <remarks>
